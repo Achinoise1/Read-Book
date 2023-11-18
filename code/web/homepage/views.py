98fa85432a595d534aa660afec3bcfd86fd740ef
Achinoise1 <87605583+Achinoise1@users.noreply.github.com>
2023-11-18 10:16:34 +0800

    主页适配

--- code/web/homepage/views.py
+++ code/web/homepage/views.py
@@ -9,7 +9,15 @@ class Index(Resource):
         try:
             temp = random.randint(0, 200)
             detail = ctrl.get_book_detail_info(temp+1)
-            return {"msg": 200, "detail": "进入首页", "data": {"html": 'index.html', "status": session['status'], "detail": detail, "bookReID": temp}}
+            print(detail)
+            wrap = {}
+            attr = ['id', 'name', 'author', 'country', 'publisher',
+                    'year', 'page', 'price', 'frame', 'category',
+                    'isbn', 'star', 'comment_num', 'brief', 'douban_bookid',
+                    'link', 'name_o', 'trans']
+            for i in range(len(attr)):
+                wrap[attr[i]] = detail[i]
+            return {"msg": 200, "detail": "进入首页", "data": wrap}
         except Exception as e:
             # return redirect('/')
             return {"msg": 500, "detail": "未初始化"}
