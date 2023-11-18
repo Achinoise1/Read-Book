37b620637ae635ea965a3d7ec169c7989931da4b
Achinoise1 <87605583+Achinoise1@users.noreply.github.com>
2023-11-18 10:17:58 +0800

    测试数据适配

--- code/web/testStatistics/__init__.py
+++ code/web/testStatistics/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 from flask_restful import Api
 from web.testStatistics.views import statistics
 
-statistics_var = Blueprint('statistics', __name__, url_prefix='/Statistics')
+statistics_var = Blueprint('statistics', __name__, url_prefix='/api')
 statistics_api = Api(statistics_var)
-statistics_api.add_resource(statistics, '/')
+statistics_api.add_resource(statistics, '/Statistics')
